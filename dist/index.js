8586a05a3fda557a8158af398ff728aea9c67124
nuintun <nuintun@qq.com>
2018-03-26 16:33:16 +0800

    :art: Update

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
  * @module inspect-attrs
  * @author nuintun
  * @license MIT
- * @version 0.0.2
+ * @version 0.0.3
  * @description An object attributes inspect tool.
  * @see https://github.com/nuintun/inspect-attrs#readme
  */
